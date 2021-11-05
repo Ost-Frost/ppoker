56d7cffffa8253e4a0d92627ddcdcd0606104473
FLX132 <93783487+FLX132@users.noreply.github.com>
2021-11-05 19:00:09 +0100

    Update ppoker.sql

--- ppoker.sql
+++ ppoker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 05. Nov 2021 um 17:50
+-- Erstellungszeit: 05. Nov 2021 um 18:58
 -- Server-Version: 10.4.21-MariaDB
 -- PHP-Version: 8.0.11
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `ppoker`
 --
+CREATE DATABASE IF NOT EXISTS `ppoker` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `ppoker`;
 
 -- --------------------------------------------------------
 
@@ -34,6 +36,13 @@ CREATE TABLE `spiele` (
   `Beschreibung` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Daten für Tabelle `spiele`
+--
+
+INSERT INTO `spiele` (`SpielID`, `Einrichtungsdatum`, `Task`, `Beschreibung`) VALUES
+(12, '2021-11-02', 'Fett einen durchziehen', 'Mehr Gras für Deutschland!!');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +54,13 @@ CREATE TABLE `spielkarte` (
   `UserID` int(11) NOT NULL,
   `Karte` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `spielkarte`
+--
+
+INSERT INTO `spielkarte` (`SpielID`, `UserID`, `Karte`) VALUES
+(12, 123, 8);
 
 -- --------------------------------------------------------
 
@@ -60,6 +76,13 @@ CREATE TABLE `user` (
   `Passwort` text NOT NULL,
   `Registrierungsdatum` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`UserID`, `Vorname`, `Nachname`, `Mail`, `Passwort`, `Registrierungsdatum`) VALUES
+(123, 'Peter', 'Popelkopf', 'Peter.Popel@gmail.com', '123', '2021-11-01');
 
 --
 -- Indizes der exportierten Tabellen
@@ -83,6 +106,17 @@ ALTER TABLE `spielkarte`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`UserID`);
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `spielkarte`
+--
+ALTER TABLE `spielkarte`
+  ADD CONSTRAINT `spielkarte_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `spielkarte_ibfk_2` FOREIGN KEY (`SpielID`) REFERENCES `spiele` (`SpielID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
