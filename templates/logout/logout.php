0969c509e52d348325f0d4bbe697dbc78450b233
Reimers <ole.reimers@fujitsu.com>
2021-11-07 20:21:37 +0100

    a few visual changes in login/logout

--- templates/logout/logout.php
+++ templates/logout/logout.php
@@ -1,4 +1,6 @@
+<h1 class="h3 mb-3 fw-normal">Sie wurden erfolgreich ausgeloggt.</h1>
 <p>
-    Sie wurden erfolgreich ausgeloggt. Sollte die automatische Weiterleitung nicht funktionieren, klicken Sie bitte
+    Sie werden in 5 Sekunden automatisch weitergeleitet.
+    Sollte die automatische Weiterleitung nicht funktionieren, klicken Sie bitte
     <a href="Login">hier</a>.
 </p>
