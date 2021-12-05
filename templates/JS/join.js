efcbfe2f182de19723e376f4222f32cb9bb9ea9c
Adam <adam.brezing@gmx.de>
2021-12-05 14:49:06 +0100

    Change Join

--- templates/JS/join.js
+++ templates/JS/join.js
@@ -1,13 +1,17 @@
-let c;
-function expand(event) {
-    if (c != null) {
-        c.children[5].classList.add("secondaryPartHidden");
-        c.children[2].classList.remove("secondaryPartHidden");
-        c.children[3].classList.add("secondaryPartHidden");
-    }
-    c = event.target;
-    c.children[5].classList.remove("secondaryPartHidden");
-    c.children[2].classList.add("secondaryPartHidden");
-    c.children[3].classList.remove("secondaryPartHidden");
-    console.log(c.children[5]);
+let s;
+function epic(event) {
+    e = event.target;
+    alert(e);
 }
+function expand(event) {
+    if (s != null) {
+        s.children[5].classList.add("hidden");
+        s.children[2].classList.remove("hidden");
+        s.children[3].classList.add("hidden");
+    }
+    s = event.target;
+    s.children[5].classList.remove("hidden");
+    s.children[2].classList.add("hidden");
+    s.children[3].classList.remove("hidden");
+}
+
